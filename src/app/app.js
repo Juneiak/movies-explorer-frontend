86c5887d8513bcf52f374284d60b48f57f81a4fa
Alan I <mr.juneiak@gmail.com>
2021-10-23 20:18:45 +0300

    hotfx eslint

--- src/app/app.js
+++ src/app/app.js
@@ -4,5 +4,4 @@ import './app.css';
 function App() {
   return <div className="app" />;
 }
-
 export default App;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,8 @@
+import React from 'react';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+  return <div className="app" />;
 }
 
 export default App;
