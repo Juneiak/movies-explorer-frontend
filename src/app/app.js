a07db3ed2dc567ccf49e8f1253dad176153ef5d8
Alan I <mr.juneiak@gmail.com>
2021-10-23 20:06:31 +0300

    start

--- src/app/app.js
+++ src/app/app.js
@@ -2,11 +2,7 @@ import React from 'react';
 import './app.css';
 
 function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
+  return <div className="app" />;
 }
 
 export default App;
