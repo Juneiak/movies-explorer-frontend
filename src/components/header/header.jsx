1aca4c06896764ad60b2fa49b0a0788db6c67f00
Alan I <mr.juneiak@gmail.com>
2021-10-26 00:19:12 +0300

    add header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,22 +2,28 @@ import React from 'react';
 import { Navigation, Account, Button } from '..';
 import { Link } from 'react-router-dom';
 import './header.css';
+import useWindowDimensions from './../../utils/customHooks/use-window-dimensions';
 
 const Header = () => {
-  const isLogin = true;
-
+  const isLogin = false;
+  const { width } = useWindowDimensions();
   return (
     <header className='header'>
       
       <Link className='header__logo-link' to='/' />
-      {isLogin && <Navigation />}
-      {isLogin && <Account />}
-      {!isLogin && 
-        <div className='header__auth'>
-          <Link className='page__link header__link'>Регестрация</Link>
-          <Button size='small' form='basic' />
-        </div>
-      }
+      {isLogin && width > 780 && <Navigation />}
+      <div className='headar__menu'>
+        {isLogin && width > 780 && <Account />}
+        {isLogin && width < 780 && <button className='header__burger'></button>}
+        {!isLogin && 
+          <div className='header__auth'>
+            <Link className='page__link header__link'>Регестрация</Link>
+            <Button size='small' form='basic' text='Войти' />
+          </div>
+        }
+      </div>
+      
+      
 
 
     </header>
--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './navigation.css';
 
 const Navigation = () => {
   return (
-    <div></div>
+    <nav className='navigation'>
+      <ul className='navigation__links-list'>
+        <NavLink to='/movies' className='page__link navigation__link' activeClassName='page__link_active'>Фильмы</NavLink>
+        <NavLink to='/saved-movies' className='page__link navigation__link' activeClassName='page__link_active'>Сохранённые фильмы</NavLink>
+      </ul>
+    </nav>
   )
 };
 
